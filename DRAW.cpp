4001a1235072c1ba9423dbd4638b6162115ab455
Anirban nayek <48054437+Anirbannayek21@users.noreply.github.com>
2021-08-05 13:24:36 +0530

    Add files via upload

---
+++ DRAW.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int totalChoice;
+	cout<<"total Dorms: ";
+	cin>>totalChoice;
+	cout<<endl;
+	int n;
+	cout<<"total students: ";
+	cin>>n;
+	cout<<endl;
+	unordered_map<int,vector<int>>m;
+	for(int id=1;id<=n;id++)
+	{
+		cout<<"preference list for student "<<id<<" :- ";
+		vector<int>list;
+		for(int j=0;j<totalChoice;j++)
+		{
+			int x;
+			cin>>x;
+			list.push_back(x);
+		}
+		m[id]=list;
+		cout<<endl;
+	}
+	
+	vector<int>vis(totalChoice+1,0);
+	
+	unordered_map<int,int>rank;
+	
+    for (int i = 1; i <=n; i++) {
+    	
+    	while(true){
+        	int num = (rand() % (n - 1 + 1)) + 1;
+        	if(rank[num]==0){
+        		rank[num] = i;
+        		break;
+			}
+		}
+    }
+    cout<<"ranks and ids: "<<endl;
+    for(int r=1;r<=n;r++)
+    {
+    	cout<<r<<" "<<rank[r]<<endl;
+	}
+	cout<<endl;
+	unordered_map<int,int>allocated;
+	
+	for(int ran=1;ran<=n;ran++)
+	{
+		int ID = rank[ran];
+		for(int choice:m[ID])
+		{
+			if(vis[choice]==0){
+				vis[choice]=1;
+				allocated[ID]=choice;
+				break;
+			}
+		}
+	}
+	
+	cout<<"allocation:- "<<endl;;
+	for(int id=1;id<=n;id++)
+	{
+		
+		cout<<id<<" "<<allocated[id]<<endl;
+	}
+	return 0;
+}
